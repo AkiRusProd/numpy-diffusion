60cba8d0cd24bd9ceda133bf38c6817d8466d665
AkiRusProd <akirusprod@gmail.com>
2022-11-26 17:03:41 +0300

    add mnist&utkface image generating examples

--- diffusion/train_mnist.py
+++ diffusion/train_mnist.py
@@ -21,6 +21,7 @@ from diffusion.optimizers import Adam
 from diffusion.losses import MSE
 from diffusion.architectures import SimpleUNet
 
+import matplotlib.pyplot as plt
 
 
 training_data = open('dataset/mnist/mnist_train.csv','r').readlines()
@@ -57,4 +58,54 @@ else:
 
 
 diffusion = Diffusion(model = SimpleUNet(image_channels = 1, image_size = 28, down_channels = (32, 64, 128), up_channels = (128, 64, 32)), timesteps = 300, beta_start = 0.0001, beta_end = 0.02, criterion = MSE(), optimizer = Adam(alpha = 2e-4))
-diffusion.train(training_inputs, epochs = 30, batch_size = 10, save_every_epochs = 1, image_path = f"images/mnist", save_path = f"diffusion/saved models/mnist_model", image_size = image_size)
+
+if not os.path.exists("diffusion/saved models/mnist_model"):
+    diffusion.train(training_inputs, epochs = 30, batch_size = 10, save_every_epochs = 1, image_path = f"images/mnist", save_path = f"diffusion/saved models/mnist_model", image_size = image_size)
+else:
+    diffusion.load("diffusion/saved models/mnist_model")
+
+
+
+# Image generating example
+x_num, y_num = 5, 5
+n = x_num * y_num
+
+generated_images, generated_images_in_time = diffusion.ddpm_denoise_sample(sample_num = n, image_size = image_size, states_step_size = 10)
+
+generated_images_grid = diffusion.get_images_set(x_num, y_num, images = generated_images, margin = 10, image_size = image_size)
+
+
+plt.imshow(generated_images_grid, cmap = 'gray')
+plt.axis('off')
+plt.title('Generated images')
+plt.show()
+
+
+# Image inpainting example
+random_images = training_inputs[np.random.randint(0, len(training_inputs), n)].reshape(n, *image_size)
+
+masks = np.ones(random_images.shape)
+masks[:, :, :14, :] = 0
+
+images_grid = diffusion.get_images_set(x_num, y_num, images = random_images, margin = 10, image_size = image_size)
+
+masked_images_grid = diffusion.get_images_set(x_num, y_num, images = random_images * masks, margin = 10, image_size = image_size)
+reconstructed_images = diffusion.ddpm_denoise_sample(orig_x = random_images, mask = masks)[0]
+reconstructed_images_grid = diffusion.get_images_set(x_num, y_num, images = reconstructed_images, margin = 10, image_size = image_size)
+
+plt.subplot(1, 3, 1)
+plt.imshow(images_grid, cmap = 'gray')
+plt.axis('off')
+plt.title('Original images')
+
+plt.subplot(1, 3, 2)
+plt.imshow(masked_images_grid, cmap = 'gray')
+plt.axis('off')
+plt.title('Masked images')
+
+plt.subplot(1, 3, 3)
+plt.imshow(reconstructed_images_grid, cmap = 'gray')
+plt.axis('off')
+plt.title('Reconstructed images')
+
+plt.show()
